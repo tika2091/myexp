75004e83905b528cdcb7b6bb096abe6ea29a1fbf
tika2091 <tikapsd@gmail.com>
2019-09-26 17:47:04 -0400

    added

---
+++ src/main/java/monthlyexp/totalexp/TotalexpApplication.java
@@ -0,0 +1,13 @@
+package monthlyexp.totalexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotalexpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TotalexpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/monthlyexp/totalexp/TotalexpApplicationTests.java
@@ -0,0 +1,16 @@
+package monthlyexp.totalexp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TotalexpApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
